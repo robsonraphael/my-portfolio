ee3f087321981821e77337a58d5cdbf588e9e8b9
Robson Raphael <79436128+robsonraphael@users.noreply.github.com>
2022-07-17 19:31:26 -0300

    DELETE component content

--- src/components/content/styled.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-import { motion } from "framer-motion";
-
-export const Container = styled.main``;
-export const Title = styled(motion.h1)``;
-export const Ani = styled(motion.p)``;
