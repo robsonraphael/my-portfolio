4b1a5a793c145371e43afb2a2434c67214e65799
Robson Raphael <robsonraphael2001@gmail.com>
2022-07-17 19:35:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 // Components
 import { Header } from "./components/header";
 import { Global } from "./style/global";
-import { Content} from "./components/content";
 
 function App() {
   return (
     <>
       <Header />
-      <Content />
       <Global />
     </>
   );
