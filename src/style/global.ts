ea90a1d419a7bfdd398f15772ab4d2f7235aff6c
Robson Raphael <robsonraphael2001@gmail.com>
2022-07-20 16:47:31 -0300

    Styled scroll-bar

--- src/style/global.ts
+++ src/style/global.ts
@@ -16,6 +16,22 @@ export const Global = createGlobalStyle`
     ul {
         list-style: none;
     }
+
+    &::-webkit-scrollbar{
+        width: 12px;
+        background-color:  #202324;
+    }
+    &::-webkit-scrollbar-track {
+        box-shadow: inset 0 0 5px #24283B;
+        border-radius: 10px;
+    }
+    &::-webkit-scrollbar-thumb {
+        background: #454a4d;
+        border-radius: 1px;
+    }
+    &::-webkit-scrollbar-thumb:hover {
+        background: rgb(89, 94, 97)
+    }
 `;
 
 // Colors
