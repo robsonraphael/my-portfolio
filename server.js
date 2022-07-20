4cdb3b418b5e4102bfed2c1dd2baef2802453d93
Robson Raphael <robsonraphael2001@gmail.com>
2022-07-19 23:56:33 -0300

    Added Server Express

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { resolve } = require("path");
+const app = express();
+
+app.use("/", express.static(resolve(__dirname, "./build")));
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) return err;
+  console.log("SERVER START 🚀")
+});
