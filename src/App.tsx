3241facd5043ed0a7f088a4a2cdc032bffa39fa8
Robson Raphael <robsonraphael2001@gmail.com>
2022-07-27 18:52:51 -0300

    Added Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 // Components
 import { Header } from "./components/header";
-import { Content } from "./components/content";
-import { Global } from "./style/global";
+import { Home } from "./components/home";
+import { About } from "./components/about";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <Content />
-      <Global />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </>
   );
 }
