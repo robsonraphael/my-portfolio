23272a3b47870c9a4daa5382bf60e2e83dcf4130
Robson Raphael <robsonraphael2001@gmail.com>
2022-07-27 18:49:06 -0300

    Update Home

---
+++ src/components/home/index.tsx
@@ -0,0 +1,59 @@
+/* Styled components */
+import { Container, Image, Text, Title, By, Ani, SocialMedia } from "./styled";
+
+/* Icons */
+import { HiCode, HiOutlineMail } from "react-icons/hi";
+import { RiBehanceLine, RiLinkedinFill } from "react-icons/ri";
+import { BsInstagram, BsTwitter } from "react-icons/bs";
+import { FaDev, FaGithub } from "react-icons/fa";
+
+export const Home: React.FC = () => {
+  return (
+    <Container>
+      <Image />
+      <Title>
+        Hii{" "}
+        <Ani
+          animate={{ rotate: [-15, 30, -15, 30, 0], scale: [1.1, 1] }}
+          transition={{ delay: 3, repeat: Infinity, repeatDelay: 5 }}
+        >
+          👋🏾
+        </Ani>
+      </Title>
+      <Text>
+        Meu nome é Robson, sou Desenvolvedor Front-end e Freelancer nas horas
+        vagas. Apaixonado por tecnologia, musica e esportes ! Tente olhar meu{" "}
+        <a href="#">portifólio</a> e me seguir nas redes sociais! 🫠
+      </Text>
+      <By>
+        <HiCode size="1.2em" /> by Robson Raphael
+      </By>
+      <SocialMedia>
+        <a href="#">
+          <RiBehanceLine />
+        </a>
+        <a href="mailto:robsonraphaelwork@gmail.com" target="_blank">
+          <HiOutlineMail />
+        </a>
+        <a href="http://www.github.com/robsonraphael" target="_blank">
+          <FaGithub />
+        </a>
+        <a href="#">
+          <BsInstagram />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/robson-raphael-42a628224/"
+          target="_blank"
+        >
+          <RiLinkedinFill />
+        </a>
+        <a href="#">
+          <FaDev />
+        </a>
+        <a href="#">
+          <BsTwitter />
+        </a>
+      </SocialMedia>
+    </Container>
+  );
+};
---
+++ src/components/home/styled.ts
@@ -0,0 +1,124 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Colors } from "../../style/global";
+
+export const Container = styled.main`
+  width: 100%;
+  height: 100vh;
+
+  background-color: ${Colors.Mirage};
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5em;
+`;
+
+export const Image = styled.img.attrs({
+  src: "https://avatars.githubusercontent.com/u/79436128?v=4",
+})`
+  width: 240px;
+  height: 240px;
+  border-radius: 50%;
+  margin-top: 1em;
+  margin-bottom: 0.9em;
+
+  @media (min-width: 768px) {
+    width: 260px;
+    height: 260px;
+  }
+`;
+
+export const Text = styled.p`
+  font-size: 0.9em;
+  color: white;
+  line-height: 1.5em;
+  letter-spacing: 0.1em;
+  width: 420px;
+
+  a {
+    font-weight: 500;
+    color: white;
+
+    &:hover {
+      border-bottom: 0.5px solid ${Colors.Comet};
+    }
+  }
+
+  @media (min-width: 768px) {
+    width: 500px;
+    font-size: 1em;
+  }
+`;
+export const Title = styled.h1`
+  color: white;
+  font-size: 1.3em;
+  letter-spacing: 0.1em;
+  display: flex;
+  gap: 0.3em;
+  color: ${Colors.Mint};
+
+  @media (min-width: 768px){
+    font-size: 1.4em;
+  }
+`;
+export const By = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.2em;
+
+  transform: rotate(90deg);
+  position: absolute;
+  left: -50px;
+  top: 50%;
+
+  color: ${Colors.Biloba};
+  font-size: 0.8em;
+  font-weight: bold;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+export const Ani = styled(motion.div)``;
+export const SocialMedia = styled.div`
+  gap: 0.5em;
+  display: flex;
+  align-items: center;
+  font-size: 1.3em;
+  margin-top: 2.5em;
+
+  a {
+    cursor: pointer;
+    transition: ease-in 0.25s;
+    text-decoration: none;
+    color: ${Colors.Portage};
+    &:hover {
+      color: ${Colors.Desert};
+    }
+
+    &:focus {
+      :nth-child(1) {
+        color: #fff;
+      }
+      :nth-child(2) {
+        color: #bb201d;
+      }
+      :nth-child(3){
+        color: black;
+      }
+      :nth-child(4){
+        color: #863ab6;
+      }
+      :nth-child(5){
+        color: #026fab;
+      }
+      :nth-child(6){
+        color: #101010;
+      }
+      :nth-child(7){
+        color: #28a0d5;
+      }
+    }
+  }
+`;
